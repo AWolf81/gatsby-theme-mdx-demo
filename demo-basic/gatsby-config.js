6458f1045780da20fc32e1191080ffb18d97ec4a
AWolf81 <AWolf2904@gmail.com>
2019-07-16 22:53:15 +0200

    Change workspaces to basic & advanced

---
+++ demo-basic/gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: '@awolf81/gatsby-theme-mdx',
+      options: {
+        /* basePath: '/myCoolStuff' */
+        /* contentPath: 'your-content-location' */
+      }
+    }
+  ]
+}
